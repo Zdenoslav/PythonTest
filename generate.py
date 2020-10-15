6eb268169d79a9c8f514fb2367d389280edae8ca
Zdenko Mucina <zdenoslav@gmail.com>
2020-10-16 00:13:45 +0200

    new data

---
+++ generate.py
@@ -0,0 +1,26 @@
+# create the testing files
+first_names = []
+for i in range(10):
+    first_names.append(names.get_first_name())
+
+last_names = []
+for i in range(10):
+    last_names.append(names.get_last_name())
+
+ids = []
+for i in range(10):
+    ids.append(random.randint(1111, 9999))
+
+departments = ["Sales", "Legal", "Defence", "Development"]
+chosenDepartments = []
+for i in range(10):
+    chosenDepartments.append(random.choice(departments))
+
+    # Generating names part
+    # print(names.get_full_name())
+
+# Creating of the files part
+f = io.open("test_data.txt", mode="w", encoding="utf-8")
+for i in range(10):
+    f.write(str(ids[i]) + ":" + first_names[i] + ":" +
+            last_names[i] + ":" + chosenDepartments[i] + "\n")
--- test.py
+++ test.py
@@ -2,39 +2,13 @@ import random
 import names
 import io
 import argparse
-import ugen
 import os
+import tempfile
 
-# create the testing files
-first_names = []
-for i in range(10):
-    first_names.append(names.get_first_name())
-
-last_names = []
-for i in range(10):
-    last_names.append(names.get_last_name())
-
-ids = []
-for i in range(10):
-    ids.append(random.randint(1111, 9999))
-
-departments = ["Sales", "Legal", "Defence", "Development"]
-chosenDepartments = []
-for i in range(10):
-    chosenDepartments.append(random.choice(departments))
-
-    # Generating names part
-    # print(names.get_full_name())
-
-# Creating of the files part
-f = io.open("test_data1.txt", mode="w", encoding="utf-8")
-for i in range(10):
-    f.write(str(ids[i]) + ":" + first_names[i] + ":" +
-            last_names[i] + ":" + chosenDepartments[i] + "\n")
 
 # run the ugen.py program and give it the input files
 
-os.system('python3 ugen.py')
+
 # read the output file and see if the data is as expected
 
 
@@ -44,3 +18,9 @@ if __name__ == '__main__':
     parser.add_argument('tested_files', type=str, help='create an output file')
     parser.add_argument('test_data', type=str)
     args = parser.parse_args()
+
+    with tempfile.NamedTemporaryFile(mode='w') as f:
+        f.write(args.test_data)
+        f.flush()
+
+        os.system('python3 ugen.py -o output.txt {}'.format(f.name))
